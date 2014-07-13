afacbe621930f05899a1c11632998b538732d35a
Chris Loew <cloew123@gmail.com>
2014-07-13 10:44:37 -0600

    Added Python Toggl setup file and initial directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pytoggl',
+      version='0.1',
+      description='Python Toggl REST API Wrapper',
+      author='Chris Loew',
+      author_email='cloew123@gmail.com',
+      #url='http://www.python.org/sigs/distutils-sig/',
+      packages=['pytoggl'],
+     )
