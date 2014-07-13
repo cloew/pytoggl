8ff3af7385f5c6c5017fb4686db00f8f040b8d5f
Chris Loew <cloew123@gmail.com>
2014-07-13 12:16:36 -0600

    Adde Time Entry class

---
+++ pytoggl/time_entry.py
@@ -0,0 +1,12 @@
+
+class TimeEntry:
+    """ Represents a Toggl Time Entry """
+    
+    def __init__(self, json=None):
+        """ Initialize a Toggl Time Entry """
+        self.id = json["id"]
+        self.description = json["description"]
+        
+    def __repr__(self):
+        """ String representation of the Time Entry """
+        return "{0}:{1}".format(self.id, self.description)
