b28add286cef8234666a92fb0a73167eb32a4863
Chris Loew <cloew123@gmail.com>
2014-07-18 20:21:07 -0600

    Can now create a new project

--- pytoggl/projects_api.py
+++ pytoggl/projects_api.py
@@ -1,3 +1,4 @@
+from project import Project
 
 class ProjectsAPI:
     """ Represents the Toggl Projects API """
@@ -6,3 +7,14 @@ class ProjectsAPI:
     def __init__(self, requestHelper):
         """ Initialize the Projects API """
         self.requestHelper = requestHelper
+        
+    def create(self, project):
+        """ Create a new project from the given project """
+        url = "{0}".format(self.baseUrl)
+        response = self.requestHelper.post(url, data=project.toJSONDictionary())
+        
+        createdProject = None
+        if response.status_code == 200 and response.json()["data"] is not None:
+            createdProject = Project(json=response.json()["data"])
+        print response.status_code, response
+        return createdProject
