2b8fd8a361f5f7e1406c3cb416d3850463fd7dca
Chris Loew <cloew123@gmail.com>
2014-07-18 20:05:30 -0600

    Refactored Project to accept keyword arguments and store the parameters in a dictionary to allow easy transfer to to the Toggl API

--- pytoggl/project.py
+++ pytoggl/project.py
@@ -1,14 +1,35 @@
-
-class Project:
+import sys
+class Project(object):
     """ Represents a Toggl Project """
     
-    def __init__(self, id=None, json=None):
+    def __init__(self, json=None, **kwargs):
         """ Initialize a Toggl Project """
+        self.__values = {}
         if json is not None:
-            self.id = json["id"]
-            self.name = json["name"]
+            self.__values = json
         else:
-            self.id = id
+            for key, value in kwargs.iteritems():
+                setattr(self, key, value)
+            
+    def toJSONDictionary(self):
+        """ Convert the Time Entry to JSON """
+        return {"project":self.__values}
+        
+    def __getattr__(self, name):
+        """ Return an attribute of the project """
+        if name == "_Project__values":
+            return self.__values
+        elif name in self.__values:
+            return self.__values[name]
+        else:
+            raise AttributeError(name)
+            
+    def __setattr__(self, name, value):
+        """ Return an attribute of the project """
+        if name == "_Project__values":
+            self.__dict__[name] = value
+        else:
+            self.__values[name] = value
         
     def __repr__(self):
         """ String representation of the project """
