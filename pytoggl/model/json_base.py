e8d8cde0e1c89c9246da4f2971770d458c026d0d
Chris Loew <cloew123@gmail.com>
2014-07-19 08:09:00 -0600

    Project now uses the JSON Base class

---
+++ pytoggl/model/json_base.py
@@ -0,0 +1,32 @@
+import sys
+class JsonBase:
+    """ Represents a JSON based class """
+    
+    def __init__(self, json=None, **kwargs):
+        """ Initialize the JSON Record """
+        self.__values = {}
+        if json is not None:
+            self.__values = json
+        else:
+            for key, value in kwargs.iteritems():
+                setattr(self, key, value)
+            
+    def toJSONDictionary(self):
+        """ Retrieve the JSON values """
+        return self.__values
+        
+    def __getattr__(self, name):
+        """ Return an attribute of the JSON Base """
+        if name == "_JsonBase__values":
+            return self.__values
+        elif name in self.__values:
+            return self.__values[name]
+        else:
+            raise AttributeError(name)
+            
+    def __setattr__(self, name, value):
+        """ Return an attribute of the JSON Base """
+        if name == "_JsonBase__values":
+            self.__dict__[name] = value
+        else:
+            self.__values[name] = value
--- pytoggl/model/project.py
+++ pytoggl/model/project.py
@@ -1,35 +1,11 @@
-import sys
-class Project(object):
+from json_base import JsonBase
+
+class Project(JsonBase):
     """ Represents a Toggl Project """
-    
-    def __init__(self, json=None, **kwargs):
-        """ Initialize a Toggl Project """
-        self.__values = {}
-        if json is not None:
-            self.__values = json
-        else:
-            for key, value in kwargs.iteritems():
-                setattr(self, key, value)
             
     def toJSONDictionary(self):
-        """ Convert the Time Entry to JSON """
-        return {"project":self.__values}
-        
-    def __getattr__(self, name):
-        """ Return an attribute of the project """
-        if name == "_Project__values":
-            return self.__values
-        elif name in self.__values:
-            return self.__values[name]
-        else:
-            raise AttributeError(name)
-            
-    def __setattr__(self, name, value):
-        """ Return an attribute of the project """
-        if name == "_Project__values":
-            self.__dict__[name] = value
-        else:
-            self.__values[name] = value
+        """ Convert the Project to JSON """
+        return {"project":JsonBase.toJSONDictionary(self)}
         
     def __repr__(self):
         """ String representation of the project """
