e90da11179146f8cfe35a9c60998de9187a9e133
Chris Loew <cloew123@gmail.com>
2014-07-26 11:21:51 -0600

    Added api package to install file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='pytoggl',
-      version='0.1',
+      version='0.2',
       description='Python Toggl REST API Wrapper',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
-      packages=['pytoggl', 'pytoggl.model'],
+      packages=['pytoggl', 'pytoggl.api', 'pytoggl.model'],
      )
