9f51856275e369a1b95bf170e2171f287a48593c
Chris Loew <cloew123@gmail.com>
2015-09-24 08:51:28 -0600

    Added basic Apis Enum to consolidate the common funcitonality of the APis

---
+++ pytoggl/api/apis.py
@@ -0,0 +1,27 @@
+from ..routes import Routes
+from ..model import Workspace
+from enum import Enum
+
+class Apis(Enum):
+    """ Represents the various Apis that can be accessed """
+    Workspaces = Workspace, Routes.Workspaces, Routes.Workspace
+    
+    def __init__(self, modelCls, listRoute, itemRoute):
+        """ Initialize the API with the List Route and Item Route """
+        self.modelCls = modelCls
+        self.listRoute = listRoute
+        self.itemRoute = itemRoute
+        
+    def all(self, apiHelper):
+        """ Return all the Model Objects for the list route """
+        json = apiHelper.get(self.listRoute.build())
+        return self.buildModels(json)
+        
+    def get(self, apiHelper, **kwargs):
+        """ Return the specific Model Object for the item route """
+        json = apiHelper.get(self.itemRoute.build(**kwargs), getData=True)
+        return self.buildModels([json])
+        
+    def buildModels(self, json):
+        """ Return the model objects to wrap the json """
+        return [self.modelCls(jsonElement) for jsonElement in json]
