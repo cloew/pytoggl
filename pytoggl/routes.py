33d608741b54ec454d33098f852a081e808f4a92
Chris Loew <cloew123@gmail.com>
2015-09-25 08:49:46 -0600

    Added project routes

--- pytoggl/routes.py
+++ pytoggl/routes.py
@@ -4,6 +4,8 @@ class Routes(Enum):
     """ Represents the various routes associated with the Toggl API """
     Workspaces = "workspaces"
     Workspace = "workspaces/{id}"
+    Projects = "workspaces/{id}/projects"
+    Project = "projects/{id}"
     
     def build(self, **kwargs):
         """ Build the Url with the given arguments """
