3ecfadae48c1e6dca52342c48466c2580330122c
Chris Loew <cloew123@gmail.com>
2015-09-25 12:46:15 -0600

    Added proper init method to the nested api decorator

--- pytoggl/api/api_for.py
+++ pytoggl/api/api_for.py
@@ -12,9 +12,9 @@ def nested_api_for(model, requestsApi):
 def base_api_for(model, requestsApi, baseCls):
     """ Add the proper init overload to properly initialize the baseCls Subclass """
     def wrap(cls):
-        def init(self, connection):
+        def init(self, connection, parent):
             """ Initialize with the keyword arguments """
-            baseCls.__init__(self, model, requestsApi, connection)
+            baseCls.__init__(self, model, requestsApi, connection, parent)
         cls.__init__ = init
         return cls
     return wrap
