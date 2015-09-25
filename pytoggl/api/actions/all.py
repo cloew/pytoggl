5cb16ca3d180a0db6a14b377b93649fe31968bd0
Chris Loew <cloew123@gmail.com>
2015-09-25 10:33:46 -0600

    Updated the all action to use the KaoDescriptor helper class

--- pytoggl/api/actions/all.py
+++ pytoggl/api/actions/all.py
@@ -1,14 +1,11 @@
+from pytoggl.helpers import KaoDescriptor
 
-class All:
+class All(KaoDescriptor):
     """ Action to return all elements of a Data Model """
     
-    def __get__(self, obj, type=None):
-        """ Return this descriptor or a method that retrieves and updates a Model's Json """
+    def _get(self, obj, type=None):
+        """ Return a method that retrieves and updates a Model's Json """
         def all():
             json = obj.jsonApi.all(obj.connection)
             return obj.buildModels(json)
-        
-        if obj is None:
-            return self
-        else:
-            return all
+        return all
