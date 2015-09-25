c5aa4b83a04955f39c7f3c584da9741482db3238
Chris Loew <cloew123@gmail.com>
2015-09-25 12:22:28 -0600

    Added Nested Api

---
+++ pytoggl/api/nested_api.py
@@ -0,0 +1,10 @@
+
+class NestedApi:
+    """ Represents an Api that is nested within another Model """
+    
+    def __init__(self, modelCls, requestApi, connection, parent):
+        """ Initialize with the Model Class, Request Api, connection and parent record """
+        self.requestApi = requestApi
+        self.connection = connection
+        self.parent = parent
+        self.buildModels = ModelWrapper(modelCls, connection)
