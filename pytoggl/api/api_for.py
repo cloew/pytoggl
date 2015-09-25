768ea1293a8dee1cbfa8eabf2245eb978d0d9a6d
Chris Loew <cloew123@gmail.com>
2015-09-25 12:33:31 -0600

    Added new decorator to wrap nested apis

--- pytoggl/api/api_for.py
+++ pytoggl/api/api_for.py
@@ -1,11 +1,20 @@
 from .model_api import ModelApi
+from .nested_api import NestedApi
 
 def api_for(model, requestsApi):
     """ Add the proper init overload to properly initialize the ModelApi Subclass """
+    return base_api_for(model, requestsApi, ModelApi)
+
+def nested_api_for(model, requestsApi):
+    """ Add the proper init overload to properly initialize the NestedApi Subclass """
+    return base_api_for(model, requestsApi, NestedApi)
+
+def base_api_for(model, requestsApi, baseCls):
+    """ Add the proper init overload to properly initialize the baseCls Subclass """
     def wrap(cls):
         def init(self, connection):
             """ Initialize with the keyword arguments """
-            ModelApi.__init__(self, model, requestsApi, connection)
+            baseCls.__init__(self, model, requestsApi, connection)
         cls.__init__ = init
         return cls
     return wrap
