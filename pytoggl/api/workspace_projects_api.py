4151d12de1680ab00cdb2a33e688db48601d3a1d
Chris Loew <cloew123@gmail.com>
2015-09-25 12:39:47 -0600

    Added Nested Api for workspace-projects

---
+++ pytoggl/api/workspace_projects_api.py
@@ -0,0 +1,11 @@
+from .actions import NestedAll, WithName
+from .api_for import nested_api_for
+from .nested_api import NestedApi
+from ..requests import RequestApis
+from ..model import Project
+
+@nested_api_for(Project, RequestApis.Projects)
+class WorkspaceProjectsApi(NestedApi):
+    """ Represents the Toggl Projects API nested within Workspaces """
+    all = NestedAll()
+    withName = WithName()
