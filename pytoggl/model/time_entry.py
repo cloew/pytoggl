8e75e48a717784384f388cafa0708471c9321213
Chris Loew <cloew123@gmail.com>
2015-09-28 11:52:59 -0600

    Updated TimeEntry duration to return a timedelta object

--- pytoggl/model/time_entry.py
+++ pytoggl/model/time_entry.py
@@ -3,6 +3,9 @@ from .model import Model
 from .model_for_api import model_for_api
 from ..requests import RequestApis
 
+from datetime import timedelta
+from time import time
+
 @model_for_api(RequestApis.TimeEntries, created_with="pytoggl")
 class TimeEntry(Model):
     """ Represents a Toggl Time Entry """
@@ -12,6 +15,14 @@ class TimeEntry(Model):
     delete = Delete()
     start = StartTimer()
     stop = StopTimer()
+    
+    @property
+    def duration(self):
+        """ Return the proper duration """
+        duration = self._data.duration
+        if duration < 0:
+            duration = time() + duration
+        return timedelta(seconds=duration)
         
     def __repr__(self):
         """ String representation of the project """
