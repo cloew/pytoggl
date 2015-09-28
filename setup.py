06ef44b90581b9d5974f6113bfa47268f55ff075
Chris Loew <cloew123@gmail.com>
2015-09-28 10:12:15 -0600

    Added missing imports

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setup(name='pytoggl',
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
-      packages=['pytoggl', 'pytoggl.api', 'pytoggl.model'],
+      packages=['pytoggl', 'pytoggl.api', 'pytoggl.api.actions', 'pytoggl.api.nested',
+                'pytoggl.helpers', 'pytoggl.model', 'pytoggl.model.actions',
+                'pytoggl.requests'],
      )
